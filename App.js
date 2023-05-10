64f25c41c66d1917fca70204ac58fe1933fe9d25
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2023-05-10 01:56:29 -0600

    primer version

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const DogAPI = () => {
+  const [breed, setBreed] = useState('');
+  const [images, setImages] = useState([]);
+  const getDogImages = async () => {
+    try {
+      const response = await axios.get(`https://dog.ceo/api/breed/${breed}/images`);
+      setImages(response.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>DogAPI</Text>
+      <TextInput style={styles.input} placeholder="Enter a breed" onChangeText={setBreed} />
+      <Button title="Search" onPress={getDogImages} />
+      {images.length > 0 && (
+        <View style={styles.imagesContainer}>
+          {images.map((image, index) => (
+            <Image key={index} source={{ uri: image }} style={styles.image} />
+          ))}
+        </View>
+      )}
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    width: '80%',
+  },
+  imagesContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '45%',
+    height: 150,
+    margin: 5,
+  },
 });
+
+export default DogAPI;
+
